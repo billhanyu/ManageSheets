7b734427ac110df589e6b27cc8a00aeb808bdd9f
Bill Yu <billyuhan@gmail.com>
2021-09-11 21:04:42 -0700

    Respect image orientation in image picker.
    Not sure how this is a thing in 2021.

--- Shared/ImagePicker.swift
+++ Shared/ImagePicker.swift
@@ -38,10 +38,25 @@ struct ImagePicker: UIViewControllerRepresentable {
      
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                parent.selectedImage = image
+                parent.selectedImage = fixOrientation(img: image)
             }
      
             parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        private func fixOrientation(img: UIImage) -> UIImage {
+            if (img.imageOrientation == .up) {
+                return img
+            }
+
+            UIGraphicsBeginImageContextWithOptions(img.size, false, img.scale)
+            let rect = CGRect(x: 0, y: 0, width: img.size.width, height: img.size.height)
+            img.draw(in: rect)
+
+            let normalizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+            UIGraphicsEndImageContext()
+
+            return normalizedImage
         }
     }
 }
